8ea24a9f0e932ae14a5565ff473d5eb441402250
Laura Antunez <lauraantunez024@gmail.com>
2024-03-11 23:26:24 -0400

    able to extract info from the modal and log it to to the console + return to the user as part of a message

--- commands/utility/questions-modal.js
+++ commands/utility/questions-modal.js
@@ -1,43 +1,50 @@
-const { ActionRowBuilder, Client, GatewayIntentBits, Events, ModalBuilder, TextInputBuilder, TextInputStyle, SlashCommandBuilder } = require('discord.js');
+const {
+  ActionRowBuilder,
+  Client,
+  GatewayIntentBits,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  SlashCommandBuilder,
+} = require("discord.js");
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-  ]})
-
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName('availability')
-  .setDescription('whens the next movie'),
+    .setName("availability")
+    .setDescription("whens the next movie"),
   async execute(interaction) {
+    const modal = new ModalBuilder()
+      .setCustomId("movieModal")
+      .setTitle(`When's the next movie`);
 
-        const modal = new ModalBuilder()
-            .setCustomId('movieModal')
-            .setTitle(`When's the next movie`);
+    const nameInput = new TextInputBuilder()
+      .setCustomId("nameInput")
+      .setLabel("What's your name (familiarly)")
+      .setStyle(TextInputStyle.Short);
 
-            const nameInput = new TextInputBuilder()
-                .setCustomId('nameInput')
-                .setLabel("What's your name (familiarly)")
-                .setStyle(TextInputStyle.Short)
+    const dateInput = new TextInputBuilder()
+      .setCustomId("dateInput")
+      .setLabel("What day are you free this week?")
+      .setStyle(TextInputStyle.Paragraph);
 
-            const dateInput = new TextInputBuilder()
-                .setCustomId('dateInput')
-                .setLabel('What day are you free this week?')
-                .setStyle(TextInputStyle.Paragraph)
+    const nameActionRow = new ActionRowBuilder().addComponents(nameInput);
+    const dateActionRow = new ActionRowBuilder().addComponents(dateInput);
 
-            const nameActionRow = new ActionRowBuilder().addComponents(nameInput);
-            const dateActionRow = new ActionRowBuilder().addComponents(dateInput);
+    modal.addComponents(nameActionRow, dateActionRow);
 
-            modal.addComponents(nameActionRow, dateActionRow);
+    await interaction.showModal(modal);
 
-            await interaction.showModal(modal)
-
-
-  }
-}
+  },
+  
+};
 
 // const client = new Client({
 //     intents: [
--- index.js
+++ index.js
@@ -1,105 +1,83 @@
 // Require the necessary discord.js classes
-const fs = require('node:fs');
-const path = require('node:path');
-const { ActionRowBuilder, Client, Collection, GatewayIntentBits, Events, ModalBuilder, TextInputBuilder, TextInputStyle, ButtonBuilder } = require('discord.js');
+const fs = require("node:fs");
+const path = require("node:path");
+const {
+  Client,
+  Collection,
+  GatewayIntentBits,
+  Events,
 
-require('dotenv').config(); 
+} = require("discord.js");
+
+require("dotenv").config();
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-		} else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
 }
 
-client.once(Events.ClientReady, readyClient => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-
-
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+client.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isModalSubmit()) return;
+    const name = interaction.fields.getTextInputValue('nameInput');
+	const date = interaction.fields.getTextInputValue('dateInput');
+	console.log(`THE NAME OF THE PERSON IS ${name} AND THEIR AVAILABLE DAY IS ${date}`);
+    if (interaction.customId === 'movieModal') {
+	    interaction.reply({ content: `Thank you ${name} for letting us know you're available: ${date}` })}
+  });
 
-	const command = interaction.client.commands.get(interaction.commandName);
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+  const command = interaction.client.commands.get(interaction.commandName);
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
 
-
-
-
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.customId === 'make-modal') {
-        // Create the modal
-        const modal = new ModalBuilder()
-            .setCustomId('myModal')
-            .setTitle('My Modal');
-
-        // Add components to modal
-
-        // Create the text input components
-        const favoriteColorInput = new TextInputBuilder()
-            .setCustomId('favoriteColorInput')
-            // The label is the prompt the user sees for this input
-            .setLabel("What's your favorite color?")
-            // Short means only a single line of text
-            .setStyle(TextInputStyle.Short);
-
-        const hobbiesInput = new TextInputBuilder()
-            .setCustomId('hobbiesInput')
-            .setLabel("What's some of your favorite hobbies?")
-            // Paragraph means multiple lines of text.
-            .setStyle(TextInputStyle.Paragraph);
-
-        // An action row only holds one text input,
-        // so you need one action row per text input.
-        const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
-        const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
-
-        // Add inputs to the modal
-        modal.addComponents(firstActionRow, secondActionRow);
-
-        // Show the modal to the user
-        await interaction.showModal(modal);
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
     }
+  }
 });
-
-
-
-});
-
 
 // Log in to Discord with your client's token
 client.login(process.env.TOKEN);
