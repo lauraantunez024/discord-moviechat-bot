4780ca927bea279d6ff0c2dbd398cf72aaf375a8
Laura Antunez <lauraantunez024@gmail.com>
2024-03-12 10:28:12 -0400

    sequelize is working and inserting data from the modal into availabilities table

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 // Require the necessary discord.js classes
 const fs = require("node:fs");
 const path = require("node:path");
-const {
-  Client,
-  Collection,
-  GatewayIntentBits,
-  Events,
-
-} = require("discord.js");
+const Sequelize = require("sequelize");
+const { Client, Collection, GatewayIntentBits, Events } = require("discord.js");
 
 require("dotenv").config();
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const sequelize = new Sequelize("database", "user", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  storage: "database.sqlite",
+});
+
+const userAvailability = sequelize.define("availability", {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  days: Sequelize.TEXT,
+});
 
 client.commands = new Collection();
 
@@ -39,17 +49,11 @@ for (const folder of commandFolders) {
 }
 
 client.once(Events.ClientReady, (readyClient) => {
+  userAvailability.sync();
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, interaction => {
-    if (!interaction.isModalSubmit()) return;
-    const name = interaction.fields.getTextInputValue('nameInput');
-	const date = interaction.fields.getTextInputValue('dateInput');
-	console.log(`THE NAME OF THE PERSON IS ${name} AND THEIR AVAILABLE DAY IS ${date}`);
-    if (interaction.customId === 'movieModal') {
-	    interaction.reply({ content: `Thank you ${name} for letting us know you're available: ${date}` })}
-  });
+//   handles general command function
 
 client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
@@ -78,6 +82,74 @@ client.on(Events.InteractionCreate, async (interaction) => {
     }
   }
 });
+
+
+// client.on(Events.InteractionCreate, async (interaction) => {
+//     if (!interaction.isChatInputCommand()) return;
+  
+//     const { commandName } = interaction;
+  
+//     if (commandName === "availability") {
+//       const nameData = interaction.fields.getTextInputValue("nameInput");
+//       const dateData = interaction.fields.getTextInputValue("dateInput");
+  
+//       try {
+//         // equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
+//         const info = await userAvailability.create({
+//           name: nameData,
+//           date: dateData,
+//         });
+  
+//         return interaction.reply(`User ${info.name} added on ${info.date}.`);
+//       } catch (error) {
+//         if (error.name === "SequelizeUniqueConstraintError") {
+//           return interaction.reply("That tag already exists.");
+//         }
+  
+//         return interaction.reply("Something went wrong with adding a tag.");
+//       }
+//     }
+//   });
+// handles modal
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isModalSubmit()) return;
+  const nameData = interaction.fields.getTextInputValue("nameInput");
+  const dateData = interaction.fields.getTextInputValue("dateInput");
+  console.log(
+    `THE NAME OF THE PERSON IS ${nameData} AND THEIR AVAILABLE DAY IS ${dateData}`
+  );
+  if (interaction.customId === "movieModal") {
+    try {
+        // equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
+        const info = await userAvailability.create({
+          name: nameData,
+          days: dateData,
+        });
+        const singleUser = await userAvailability.findOne({ where: { name: info.name } })
+
+        if (singleUser) {
+
+            return interaction.reply(singleUser.get('days'));
+        }
+        return interaction.reply(`Could not find tag: ${singleUser}`);
+
+
+  
+      } catch (error) {
+        if (error.name === "SequelizeUniqueConstraintError") {
+          return interaction.reply("Something already exists.");
+        }
+  
+        return interaction.reply("Something went wrong :(");
+      }
+
+
+    
+  }
+});
+
+
 
 // Log in to Discord with your client's token
 client.login(process.env.TOKEN);
